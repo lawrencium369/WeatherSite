23b5c4b64d7110a5b223e23a35cae4ad08de9f9b
lawrence musonda <lawrencemusonda555@gmail.com>
2025-07-17 15:44:32 +0100

    fixed a bug

---
+++ main.js
@@ -0,0 +1,25 @@
+const myForm = document.querySelector('#my-form');
+const cityName = document.querySelector('#cityName');
+const msg = document.querySelector('#msg');
+const inputField = document.querySelector('#cityName');
+
+let cities = ['rabat', 'fes'];
+
+myForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if (cityName.value === '') {
+    inputField.style.backgroundColor = 'red';
+    setTimeout(() => inputField.style.backgroundColor = '#dee3dd', 2000);
+  } else {
+    const enteredCity = inputField.value.toLowerCase();
+    if (cities.includes(enteredCity)) {
+      localStorage.setItem('selectedCity', enteredCity); // Store in localStorage
+      window.location.href = 'city.html'; // Navigate
+    } else {
+      msg.textContent = 'City not recognized.';
+    }
+  }
+
+  inputField.value = '';
+});
