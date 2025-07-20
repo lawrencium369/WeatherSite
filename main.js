38f0167b84600da0df67043d62712866c4920eaf
lawrence musonda <lawrencemusonda555@gmail.com>
2025-07-20 01:20:07 +0100

    added the display function

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 const myForm = document.querySelector('#my-form');
-const cityName = document.querySelector('#cityName');
-const msg = document.querySelector('#msg');
-const inputField = document.querySelector('#cityName');
+const Display = document.querySelector('.Display');
+const cityInput = document.querySelector('#cityName');
 const api_key = "d6ddc59f0b2555f271326ca7a6fd3f4e";
 
-myForm.addEventListener('submit', function(e) {
-  e.preventDefault();
+myForm.addEventListener('submit', async event => {
+    event.preventDefault();
 
-  if (cityName.value === '') {
-    inputField.style.backgroundColor = 'red';
-    setTimeout(() => inputField.style.backgroundColor = '#dee3dd', 2000);
-  } else {
-    const enteredCity = inputField.value.toLowerCase();
-    if (cities.includes(enteredCity)) {
-      localStorage.setItem('selectedCity', enteredCity); // Store in localStorage
-      window.location.href = 'city.html'; // Navigate
-    } else {
-      msg.textContent = 'City not recognized.';
-      setTimeout(() => msg.textContent = '', 2000);
+    const city = cityInput.value;
+
+    if(city){
+        try{
+          const weatherData = await getWeatherData(city);
+          showData(weatherData);
+        }
+        catch(error){
+          console.error(error);
+        }
+        
     }
-  }
 
   inputField.value = '';
 });
@@ -34,3 +32,31 @@ async function getWeatherData(city){
   return response.json();
 }
 
+function showData(data){
+  const {name: city,
+         main: {temp, humidity}, 
+         weather: [{description, id}] } = data;
+  
+  Display.textContent = "";
+  Display.style.display = "flex";
+
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const showDescription = document.createElement("p");
+
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `temperature: ${(temp - 273.15).toFixed(1)}°C`;
+  humidityDisplay.textContent = `Humidity: ${humidity}`;
+  showDescription.textContent = description;
+
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  showDescription.classList.add("Description");
+
+  Display.appendChild(cityDisplay);
+  Display.appendChild(tempDisplay);
+  Display.appendChild(humidityDisplay);
+  Display.appendChild(showDescription);
+}
